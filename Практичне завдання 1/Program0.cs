7a58a82f20b4fe03bd50b340f2831a7669e99c70
paxawok <91912688+paxawok@users.noreply.github.com>
2022-09-29 16:07:49 +0300

    Add project files.

---
+++ Практичне завдання 1/Program0.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Practice
+{
+    public static void Main()
+    {
+        Random random = new Random(1234);
+        for (int y = 0; y < 10; y++)
+        {
+            Console.WriteLine("{0,3} ", random.Next(1, 10));
+        } 
+    }
+}
---
+++ Практичне завдання 1/Program1.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+
+public class RandomCrypto
+{
+    public static byte[] GenerateRandomNumber()
+    {
+        using (var randomNumberGenerator = RandomNumberGenerator.Create())
+        {
+            int length = 64;
+            var randomNumber = new byte[length];
+            randomNumberGenerator.GetBytes(randomNumber);
+            return randomNumber;
+        }
+    }
+    public static void Main()
+    {
+        int c = 1;
+        for (int i = 0; i < 20; i++)
+        {
+            var rnd = GenerateRandomNumber();
+            Console.WriteLine(c + ". " + Convert.ToBase64String(rnd) + "\n");
+            c++;
+        }
+        Console.ReadLine();
+    }
+}
