dba0bc86d76966a54646d1be84aea06307a9f888
paxawok <91912688+paxawok@users.noreply.github.com>
2022-11-28 07:59:56 +0200

    Зберігання інформації (sym)
    Зберігання конфіденційної інформації використовуючи методи симетричного шифрування (ПЗ 5)

---
+++ Практичне завдання 5/pz5/5_1.cs
@@ -0,0 +1,231 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        do
+        {
+            Console.WriteLine("\nMenu");
+            Console.WriteLine("____________________\n");
+            Console.WriteLine("1. DES");
+            Console.WriteLine("2. Triple-DES");
+            Console.WriteLine("3. AES");
+            Console.WriteLine("4. Exit\n");
+            Console.WriteLine("_____________________\n");
+            Console.WriteLine("Choose: ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter your data");
+                    Console.WriteLine("Your message: ");
+                    string uMess = Console.ReadLine();
+
+                    var des = new desEncryption();
+                    var key = des.GenerateRandomNumber(8);
+                    var iv = des.GenerateRandomNumber(8);
+
+                    var logForEnc = des.Encryption(Encoding.UTF8.GetBytes(uMess), key, iv);
+                    var encMessage = Convert.ToBase64String(logForEnc);
+                    var logForDec = des.Decryption(logForEnc, key, iv);
+                    var decMessage = Encoding.UTF8.GetString(logForDec);
+
+                    Console.WriteLine("DES: \n");
+                    Console.WriteLine("____________________________________\n");
+                    Console.WriteLine("Original message ->  " + uMess);
+                    Console.WriteLine("Encrypted message -> " + encMessage);
+                    Console.WriteLine("Decrypted message -> " + decMessage);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter your data");
+                    Console.WriteLine("Your message: ");
+                    string uMess1 = Console.ReadLine();
+
+                    var tdes = new tripleDesEncryption();
+                    var key1 = tdes.GenerateRandomNumber(16);
+                    var iv1 = tdes.GenerateRandomNumber(8);
+
+                    var logForEnc1 = tdes.Encryption(Encoding.UTF8.GetBytes(uMess1), key1, iv1);
+                    var encMessage1 = Convert.ToBase64String(logForEnc1);
+                    var logForDec1 = tdes.Decryption(logForEnc1, key1, iv1);
+                    var decMessage1 = Encoding.UTF8.GetString(logForDec1);
+
+                    Console.WriteLine("Triple DES: \n");
+                    Console.WriteLine("____________________________________\n");
+                    Console.WriteLine("Original message ->  " + uMess1);
+                    Console.WriteLine("Encrypted message -> " + encMessage1);
+                    Console.WriteLine("Decrypted message -> " + decMessage1);
+                    break;
+                case 3:
+                    Console.WriteLine("Enter your data");
+                    Console.WriteLine("Your message: ");
+                    string uMess2 = Console.ReadLine();
+
+                    var aes = new aesEncryption();
+                    var key2 = aes.GenerateRandomNumber(32);
+                    var iv2 = aes.GenerateRandomNumber(16);
+
+                    var logForEnc2 = aes.Encryption(Encoding.UTF8.GetBytes(uMess2), key2, iv2);
+                    var encMessage2 = Convert.ToBase64String(logForEnc2);
+                    var logForDec2 = aes.Decryption(logForEnc2, key2, iv2);
+                    var decMessage2 = Encoding.UTF8.GetString(logForDec2);
+
+                    Console.WriteLine("AES: \n");
+                    Console.WriteLine("____________________________________\n");
+                    Console.WriteLine("Original message ->  " + uMess2);
+                    Console.WriteLine("Encrypted message -> " + encMessage2);
+                    Console.WriteLine("Decrypted message -> " + decMessage2);
+                    break;
+                case 4:
+                    Environment.Exit(0);
+                    break;
+            }
+        } while (true);
+    }
+}
+
+class desEncryption
+{
+    public byte[] GenerateRandomNumber(int length)
+    {
+        using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+        {
+            var randomNumber = new byte[length];
+            randomNumberGenerator.GetBytes(randomNumber);
+            return randomNumber;
+        }
+    }
+    public byte[] Encryption (byte[] dataToEncrypt, byte[] key, byte[] iv)
+    {
+        using (var des = new DESCryptoServiceProvider())
+        {
+            des.Mode = CipherMode.CBC;
+            des.Padding = PaddingMode.PKCS7;
+            des.Key = key;
+            des.IV = iv;
+            using (var memoryStream = new MemoryStream())
+            {
+                var cryptoStream = new CryptoStream(memoryStream, des.CreateEncryptor(), CryptoStreamMode.Write);
+                cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
+                cryptoStream.FlushFinalBlock();
+                return memoryStream.ToArray();
+            }
+        }
+    }
+    public byte[] Decryption(byte[] dataToDecrypt, byte[] key, byte[] iv)
+    {
+        using (var des = new DESCryptoServiceProvider())
+        {
+            des.Mode = CipherMode.CBC;
+            des.Padding = PaddingMode.PKCS7;
+            des.Key = key;
+            des.IV = iv;
+            using (var memoryStream = new MemoryStream())
+            {
+                var cryptoStream = new CryptoStream(memoryStream, des.CreateDecryptor(), CryptoStreamMode.Write);
+                cryptoStream.Write(dataToDecrypt, 0, dataToDecrypt.Length);
+                cryptoStream.FlushFinalBlock();
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
+class tripleDesEncryption
+{
+    public byte[] GenerateRandomNumber(int length)
+    {
+        using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+        {
+            var randomNumber = new byte[length];
+            randomNumberGenerator.GetBytes(randomNumber);
+            return randomNumber;
+        }
+    }
+    public byte[] Encryption(byte[] dataToEncrypt, byte[] key, byte[] iv)
+    {
+        using (var des = new TripleDESCryptoServiceProvider())
+        {
+            des.Mode = CipherMode.CBC;
+            des.Padding = PaddingMode.PKCS7;
+            des.Key = key;
+            des.IV = iv;
+            using (var memoryStream = new MemoryStream())
+            {
+                var cryptoStream = new CryptoStream(memoryStream, des.CreateEncryptor(), CryptoStreamMode.Write);
+                cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
+                cryptoStream.FlushFinalBlock();
+                return memoryStream.ToArray();
+            }
+        }
+    }
+
+    public byte[] Decryption(byte[] dataToDecrypt, byte[] key, byte[] iv)
+    {
+        using (var des = new TripleDESCryptoServiceProvider())
+        {
+            des.Mode = CipherMode.CBC;
+            des.Padding = PaddingMode.PKCS7;
+            des.Key = key;
+            des.IV = iv;
+            using (var memoryStream = new MemoryStream())
+            {
+                var cryptoStream = new CryptoStream(memoryStream, des.CreateDecryptor(), CryptoStreamMode.Write);
+                cryptoStream.Write(dataToDecrypt, 0, dataToDecrypt.Length);
+                cryptoStream.FlushFinalBlock();
+                return memoryStream.ToArray();
+            }
+        }
+    }
+
+}
+
+
+class aesEncryption
+{
+    public byte[] GenerateRandomNumber(int length)
+    {
+        using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+        {
+            var randomNumber = new byte[length];
+            randomNumberGenerator.GetBytes(randomNumber);
+            return randomNumber;
+        }
+    }
+    public byte[] Encryption(byte[] dataToEncrypt, byte[] key, byte[] iv)
+    {
+        using (var aes = new AesCryptoServiceProvider())
+        {
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Key = key;
+            aes.IV = iv;
+            using (var memoryStream = new MemoryStream())
+            {
+                var cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
+                cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
+                cryptoStream.FlushFinalBlock();
+                return memoryStream.ToArray();
+            }
+        }
+    }
+
+    public byte[] Decryption(byte[] dataToDecrypt, byte[] key, byte[] iv)
+    {
+        using (var des = new AesCryptoServiceProvider())
+        {
+            des.Mode = CipherMode.CBC;
+            des.Padding = PaddingMode.PKCS7;
+            des.Key = key;
+            des.IV = iv;
+            using (var memoryStream = new MemoryStream())
+            {
+                var cryptoStream = new CryptoStream(memoryStream, des.CreateDecryptor(), CryptoStreamMode.Write);
+                cryptoStream.Write(dataToDecrypt, 0, dataToDecrypt.Length);
+                cryptoStream.FlushFinalBlock();
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
---
+++ Практичне завдання 5/pz5_2/5_2.cs
@@ -0,0 +1,175 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    { //menu
+        do
+        {
+            Console.WriteLine("\nMenu");
+            Console.WriteLine("____________________\n");
+            Console.WriteLine("1. DES");
+            Console.WriteLine("2. Triple-DES");
+            Console.WriteLine("3. AES");
+            Console.WriteLine("4. Exit\n");
+            Console.WriteLine("_____________________\n");
+            Console.WriteLine("Choose: ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nEnter text: ");
+            string inputData = Console.ReadLine();
+            byte[] dataForUse = Encoding.UTF8.GetBytes(inputData);
+            Console.WriteLine("\nEnter the password for your text: ");
+            string uPass = Console.ReadLine();
+            //hash паролю із сіллю
+            byte[] hash = PBKDF2.hashPassword(uPass, 250000, generateRandomNumber(32));
+
+
+            var init = new Algorithms();
+            SymmetricAlgorithm algorithm;
+
+            switch (option)
+            {
+                case 1:
+                    var key1 = Key(8, hash);
+                    var iv1 = Iv(8, hash);
+
+                    algorithm = DESCryptoServiceProvider.Create();
+
+                    var encDESMessage = init.Encryption(dataForUse, key1, iv1, algorithm);
+                    var decDESMessage =init.Decryption(encDESMessage, key1, iv1, algorithm);
+
+                    Console.WriteLine("DES: \n");
+                    Console.WriteLine("____________________________________\n");
+                    Console.WriteLine("Original message ->  " + inputData);
+                    Console.WriteLine("Encrypted message -> " + Convert.ToBase64String(encDESMessage));
+                    Console.WriteLine("Decrypted message -> " + Encoding.UTF8.GetString(decDESMessage));
+                    break;
+                case 2:
+                    var key2 = Key(16, hash);
+                    var iv2 = Iv(8, hash);
+
+                    algorithm = TripleDESCryptoServiceProvider.Create();
+
+                    var encTripleDESMessage = init.Encryption(dataForUse, key2, iv2, algorithm);
+                    var decTripleDESMessage = init.Decryption(encTripleDESMessage, key2, iv2, algorithm);
+
+                    Console.WriteLine("Triple DES: \n");
+                    Console.WriteLine("____________________________________\n");
+                    Console.WriteLine("Original message ->  " + inputData);
+                    Console.WriteLine("Encrypted message -> " + Convert.ToBase64String(encTripleDESMessage));
+                    Console.WriteLine("Decrypted message -> " + Encoding.UTF8.GetString(decTripleDESMessage));
+                    break;
+                case 3:
+                    var key3 = Key(32, hash);
+                    var iv3 = Iv(16, hash);
+
+                    algorithm = AesCryptoServiceProvider.Create();
+
+                    var encAesMessage = init.Encryption(dataForUse, key3, iv3, algorithm);
+                    var decAesMessage = init.Decryption(encAesMessage, key3, iv3, algorithm);
+
+                    Console.WriteLine("AES: \n");
+                    Console.WriteLine("____________________________________\n");
+                    Console.WriteLine("Original message ->  " + inputData);
+                    Console.WriteLine("Encrypted message -> " + Convert.ToBase64String(encAesMessage));
+                    Console.WriteLine("Decrypted message -> " + Encoding.UTF8.GetString(decAesMessage));
+                    break;
+                case 4:
+                    Environment.Exit(0);
+                    break;
+            }
+        } while (true);
+    }
+    //генерація рандомних чисел
+    public static byte[] generateRandomNumber(int length)
+    {
+        using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+        {
+            var randomNumber = new byte[length];
+            randomNumberGenerator.GetBytes(randomNumber);
+            return randomNumber;
+        }
+    }
+    //функція для ключа
+    public static byte[] Key(int length, byte[] hash)
+    {
+        var KEY = new byte[length];
+        int key = 0;
+        for (int i = 0; i < length; i++)
+        {
+            KEY[key] = hash[i];
+            key++;
+        }
+        return KEY;
+    }
+    //функція для вектору ініціалізації
+    public static byte[] Iv(int length, byte[] hash)
+    {
+        var IV = new byte[length];
+        int iv = 0;
+        for (int i = hash.Length - 1; i != hash.Length - length; i--)
+        {
+            IV[iv] = hash[i];
+            iv++;
+        }
+        return IV;
+    }
+}
+//алгоритми
+public class Algorithms
+{
+    public byte[] Encryption(byte[] toEncrypt, byte[] key, byte[] iv, SymmetricAlgorithm symmetricAlgorithm)
+    {
+        using (var ag = symmetricAlgorithm)
+        {
+            ag.Mode = CipherMode.CBC;
+            ag.Padding = PaddingMode.PKCS7;
+            ag.Key = key;
+            ag.IV = iv;
+            using (var memoryStream = new MemoryStream())
+            {
+                var cryptoStream = new CryptoStream(memoryStream, ag.CreateEncryptor(), CryptoStreamMode.Write);
+                cryptoStream.Write(toEncrypt, 0, toEncrypt.Length);
+                cryptoStream.FlushFinalBlock();
+                return memoryStream.ToArray();
+            }
+        }
+    }
+
+    public byte[] Decryption(byte[] toDecrypt, byte[] key, byte[] iv, SymmetricAlgorithm symmetricAlgorithm)
+    {
+        using (var ag = symmetricAlgorithm)
+        {
+            ag.Mode = CipherMode.CBC;
+            ag.Padding = PaddingMode.PKCS7;
+            ag.Key = key;
+            ag.IV = iv;
+            using (var memoryStream = new MemoryStream())
+            {
+                var cryptoStream = new CryptoStream(memoryStream, ag.CreateDecryptor(), CryptoStreamMode.Write);
+                cryptoStream.Write(toDecrypt, 0, toDecrypt.Length);
+                cryptoStream.FlushFinalBlock();
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
+//ключ на основі пароля
+public class PBKDF2
+{
+    public static byte[] hashPassword(string passwordToHash, int numOfRounds, byte[] generatedSalt)
+    {
+        var hashedPassword = hashPasswordHash(Encoding.UTF8.GetBytes(passwordToHash), generatedSalt, numOfRounds);
+        return hashedPassword;
+    }
+
+    public static byte[] hashPasswordHash(byte[] toBeHashed, byte[] generatedSalt, int numOfRounds)
+    {
+        using (var rfc2898 = new Rfc2898DeriveBytes(toBeHashed, generatedSalt, numOfRounds, HashAlgorithmName.SHA256))
+        {
+            return rfc2898.GetBytes(32);
+        }
+    }
+}
